6a571b891f310ba7baa6d2520e6b04db4e5d205e
Michael Albertsen <culex@culex.dk>
2021-03-26 16:56:25 +0100

    Update spotifyapi-block.js

--- assets/js/spotifyapi-block.js
+++ assets/js/spotifyapi-block.js
@@ -22,42 +22,54 @@ $(document).ready(function(){
 				var count = 0;
 				$('.spotify-block-main').html(data);
 				$('.spotifyholdermain').slick({
-				  slidesToShow: 1,
-				  slidesToScroll: 1,
-				  autoplay: true,
-				  autoplaySpeed: 4000,
-				  //dots:true,
-				  fade:true,
-				  focusOnSelect:true,
-				  pauseOnFocus:true,
-				  pauseOnHover:true,
-				  //vertical:true,
-				  centerMode: true,
-					responsive: [
-					{
+					  slidesToShow: 1,
+					  slidesToScroll: 1,
+					  autoplay: true,
+					  autoplaySpeed: 4000,
+					  //dots:true,
+					  arrows:true,
+						prevArrow:"<img class='a-left control-c prev slick-prev' src='"+themeurl+"/modules/spotifyapi/assets/images/back.png'>",
+						nextArrow:"<img class='a-right control-c next slick-next' src='"+themeurl+"/modules/spotifyapi/assets/images/next.png'>",
+					  fade:true,
+					  focusOnSelect:true,
+					  pauseOnFocus:true,
+					  pauseOnHover:true,
+					  //vertical:true,
+					  centerMode: true,
+						responsive: [
+						{
 
-					  breakpoint: 1024,
-					  settings: {
-						slidesToShow: 1,
-						infinite: true
-					  }
+							breakpoint: 1024,
+							settings: {
+								slidesToShow: 1,
+								infinite: true
+							}
 
-					}, {
+						}, 	{
 
-					  breakpoint: 600,
-					  settings: {
-						slidesToShow: 1,
-						dots: true
-					  }
+							breakpoint: 600,
+							settings: {
+								slidesToShow: 1,
+								//dots: true
+							}
 
-					}, {
+						}, {
 
-					  breakpoint: 300,
-					  settings: "unslick" // destroys slick
+							breakpoint: 300,
+							settings: "unslick" // destroys slick
 
-					}
-					]
-				});		
+							}
+						]
+				});
+
+				
+				// Adjusting slick css
+				$(".slick-prev, .slick-next").css({
+					"height": "15px", 
+					"width": "15px"
+				});			
+				$(".slick-next").css("right","-20px");
+				$(".slick-prev").css("left", "-20px");
 			}
 				//var sliderItemsTotal = $('.owl-item').length;
 				//console.log(sliderItemsTotal);
