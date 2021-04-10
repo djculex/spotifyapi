9d5f914dbeb7c193a267199511099e7967659eb1
Michael Albertsen <culex@culex.dk>
2021-04-10 16:20:49 +0200

    Update index.php
    show 7 days chart of most played songs

--- index.php
+++ index.php
@@ -1,37 +1,67 @@
 <?php
-/*
- * You may not change or alter any portion of this comment or credits
- * of supporting developers from this source code or any supporting source code
- * which is considered copyrighted (c) material of the original comment or credit authors.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- */
 
-/**
- * @copyright      {@link https://xoops.org/ XOOPS Project}
- * @license        {@link https://www.gnu.org/licenses/gpl-2.0.html GNU GPL 2 or later}
- * @package
- * @since
- * @author         XOOPS Development Team
- */
 use XoopsModules\Spotifyapi\{
 	Session,
-	SpotifyWebAPI
+	SpotifyWebAPI,
+	db
 };
-include __DIR__ . '/preloads/autoloader.php';
+
+use XoopsModules\Spotifyapi;
+use XoopsModules\Spotifyapi\Constants;
+
 require_once dirname(__DIR__,2) . '/mainfile.php';
-//require_once XOOPS_ROOT_PATH . '/class/template.php';
-require_once __DIR__ . '/header.php';
-	$block = [];
-	//$tpl = new \XoopsTpl();
-	//$tpl->caching = 0;
-	//$result = b_spotifyapi_show_recently_played();
-	$block = file_get_contents(XOOPS_ROOT_PATH."/modules/spotifyapi/agent.php");
-		print_r($block);
-	//$tpl->assign('block', $result);	
-	//$tpl->display(XOOPS_ROOT_PATH . "/modules/system/templates/blocks/system_block_online.tpl");
-	//$tpl->display('db:system_block_online.tpl');
-	//break;
-    //return $block;
+include __DIR__ . '/preloads/autoloader.php';
+require_once __DIR__ . '/include/common.php';
+
+$GLOBALS['xoopsOption']['template_main'] = 'spotifyapi_indexfile.tpl';
+
+$GLOBALS['xoopsLogger']->activated = false;
+
+include __DIR__ . '/header.php';
+include XOOPS_ROOT_PATH.'/header.php';
+
+$helper = \XoopsModules\Spotifyapi\Helper::getInstance();
+$timez = $helper->getConfig('spotifyapitimezones');
+
+// Bootstrap CSS
+$GLOBALS['xoTheme']->addStylesheet($helper->url('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'));
+$GLOBALS['xoTheme']->addScript($helper->url('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'));
+
+$d = new db();
+date_default_timezone_set($timez);
+
+$td = $d->getTop();
+$yd = $d->getLwTop();
+
+$chart = [];
+$i = 0;
+
+foreach($td as $tv) {
+	$chart[$i]['lw'] = _SPOTIFYAPI_NEWCHARTENTRY;
+	foreach ($yd as $yv) {
+		$chart[$i]['tw'] = (int) $tv['pos'];
+		 		
+		if ($tv['artist'] == $yv['artist'] AND $tv['title'] == $yv['title']){
+			$chart[$i]['lw'] = (int) $yv['pos']; 
+		} 
+		$chart[$i]['artist'] = $tv['artist']; 
+		$chart[$i]['title'] = $tv['title']; 
+		$chart[$i]['image'] = $tv['image']; 
+		$chart[$i]['album'] = $tv['album']; 
+		$chart[$i]['year'] = (int) $tv['releaseyear']; 
+		$chart[$i]['artlink'] = $tv['artistlink'];
+		$chart[$i]['pop'] = (int) $tv['popularity']; 		
+	}
+	$i += 1;
+}
+
+
+
+$sub = sprintf(
+	_SPOTIFYAPI_CHARTSUBTITLE, 
+	date_format(date_create_from_format('d-m-Y H:i:s', $d->lastweek), 'd-m-Y'), 
+	date_format(date_create_from_format('d-m-Y H:i:s', $d->today), 'd-m-Y')
+);
+$GLOBALS['xoopsTpl']->assign('subtitle',$sub);
+$GLOBALS['xoopsTpl']->assign('chart', $chart);	
+include XOOPS_ROOT_PATH.'/footer.php';
