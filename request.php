d13e1fb64dc1229fc58a6be07ae52b617c59eae0
Michael Albertsen <culex@culex.dk>
2021-11-25 13:49:13 +0100

    Update request.php
    testing cors protection

--- request.php
+++ request.php
@@ -10,6 +10,7 @@ use XoopsModules\Spotifyapi\Constants;
 require_once dirname(__DIR__,2) . '/mainfile.php';
 include __DIR__ . '/preloads/autoloader.php';
 require_once __DIR__ . '/include/common.php';
+require_once __DIR__ . '/include/functions.php';
 require_once XOOPS_ROOT_PATH . '/class/template.php';
 require_once XOOPS_ROOT_PATH . '/modules/spotifyapi/blocks/recentlyplayed.php';
 $helper = \XoopsModules\Spotifyapi\Helper::getInstance();
@@ -19,6 +20,7 @@ $helper = \XoopsModules\Spotifyapi\Helper::getInstance();
 	$result = b_spotifyapi_show_recently_played();
 	$tpl->assign('block', $result);
 	$tpl->display(XOOPS_ROOT_PATH . "/modules/spotifyapi/templates/blocks/spotify_recentlyplayed_block.tpl");
+	spotifyapi_cors();
 	//$tpl->display('db:spotify_recentlyplayed_block');
 
 $GLOBALS['xoopsLogger']->activated = false;
