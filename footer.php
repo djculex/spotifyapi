47849f66f85afb5fd614adc6fc19ae8698d851c6
Michael Albertsen <culex@culex.dk>
2023-02-26 15:31:45 +0100

    Fix: Spelling

--- footer.php
+++ footer.php
@@ -9,12 +9,3 @@
  * @since      1.0
  * @min_xoops  2.5.9
  */
-
-/**
- * @copyright       The XUUPS Project http://sourceforge.net/projects/xuups/
- * @license         GNU GPL 2 or later (https://www.gnu.org/licenses/gpl-2.0.html)
- * @package         NEWBB
- * @since           4.3
- * @author          irmtfan <irmtfan@yahoo.com>
- * @author          The Persian Xoops Support Site<www.xoops.ir>
- */
