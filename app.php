161fece2408c9d512dc43bb4b82a3c04c512c5a3
Michael Albertsen <culex@culex.dk>
2023-03-15 18:28:34 +0100

    *#&!! accidentally misspelled var name using capital start letter.

--- app.php
+++ app.php
@@ -100,6 +100,6 @@ header("Access-Control-Max-Age: 1000");
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
 header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
 
-//header("Content-type: application/javascript");
-//echo json_encode($value, true);
+header("Content-type: application/javascript");
+echo json_encode($value, true);
 die();
